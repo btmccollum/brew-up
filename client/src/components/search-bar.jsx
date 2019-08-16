f580f548410fb1de29eab2097e040726f00ebd88
btmccollum <btmcco8@gmail.com>
2019-08-15 19:53:33 -0500

    adding start of searchbar component, removing default app code

---
+++ client/src/components/search-bar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+  state = {
+    query: ''
+  }
+
+  render() {
+    return(
+      <div>Hello!</div>
+    )
+  }
+}
+
+export default SearchBar;
