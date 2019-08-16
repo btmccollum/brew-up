f1be99349e3d3df814b017b9ee464596d0fb72be
btmccollum <btmcco8@gmail.com>
2019-08-16 16:27:03 -0500

    Adding material-ui and building out simple Search Query component

--- client/src/components/search-bar.jsx
+++ client/src/components/search-bar.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import { FormControl, FormHelperText, InputLabel, Input, Button } from '@material-ui/core'
 
-class SearchBar extends React.Component {
-  state = {
-    query: ''
+export default function SearchBar() {
+  const [query, setQuery] = React.useState('');
+
+  function handleChange(event) {
+    setQuery(event.target.value);
   }
 
-  render() {
-    return(
-      <div>Hello!</div>
-    )
+  function handleClick() {
+    console.log(query)
   }
+
+  return(
+    <div className="search-bar-container">
+      <form>
+        <FormControl>
+          <InputLabel htmlFor="query">Enter a state...</InputLabel>
+          <Input name="query-field" value={query} onChange={handleChange} aria-describedby="my-helper-text" />
+          <FormHelperText id="search-helper-text">Let's get you started.</FormHelperText>
+        </FormControl>
+      </form>
+
+      {/* NOTE: added for state exposure testing */}
+      {/* <Button onClick={handleClick}>
+        Show me the state!
+      </Button> */}
+    </div>
+  )
 }
-
-export default SearchBar;
